3113d31f5d572990f6215d8715f0e946459e9eab
IGlautier <ivanglautier@gmail.com>
2017-05-26 13:49:46 +0100

    Emit test

--- test/test.js
+++ test/test.js
@@ -23,11 +23,20 @@ describe("Start", function() {
   });
 });
 
-/*describe("Emit", function() {
+describe("Emit", function() {
+  this.timeout(10000);
   it("Should emit data", function() {
-
+    return new Promise((resolve, reject) => {
+      cozirDriver.on("data", (feedId, data) => {
+        if (feedId === testConfig.feedId) {
+          resolve();
+        } else {
+          reject();
+        }
+      });
+    }).should.eventually.be.fulfilled;
   });
-});*/
+});
 
 describe("Stop", function() {
   it("Should stop", function() {
